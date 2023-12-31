8370b1ceec5600ddb5c84d02928c8c5dd280d6de
angskyee <149351121+angskyee@users.noreply.github.com>
2023-12-31 19:01:11 +0900

    Set InteractionManager
    플레이어가 상호작용 할때 관리 cs 정리
    레이케스트에 닿았을때 해당 아이템 정보 UI 출력

--- Assets/Scripts/Items/InteractionManager.cs
+++ Assets/Scripts/Items/InteractionManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public interface IInteractable
@@ -10,10 +12,51 @@ public interface IInteractable
 public class InteractionManager : MonoBehaviour
 {
     public float checkLate = 0.05f;
-    private float LastCheckTIme;
+    private float lastCheckTIme;
     public float maxCheckDistance;
     public LayerMask layerMask;
 
     private GameObject curInteractGameObject;
     private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastCheckTIme > checkLate)
+        {
+            lastCheckTIme = Time.time;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curInteractable.GetInteractPrompt());
+    }
 }
