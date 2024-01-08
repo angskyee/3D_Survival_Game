6b7de1697da9621cb38e7c169879acc488c6baba
angskyee <149351121+angskyee@users.noreply.github.com>
2024-01-08 19:48:27 +0900

    Equip
    이큅 관리

--- Assets/Scriptable Object/Scripts/ItemData.cs
+++ Assets/Scriptable Object/Scripts/ItemData.cs
@@ -38,5 +38,8 @@ public class ItemData : ScriptableObject
 
     [Header("Consumable")]
     public ItemDataConsumable[] consumables;
+    
+    [Header("Equip")]
+    public GameObject equipPrefab;
 
 }
